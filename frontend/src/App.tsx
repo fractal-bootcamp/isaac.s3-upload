db62c8f2bffa73b2fa182a5d235baa1d7e71093d
Isaac Gemal <isaacgemal@gmail.com>
2024-10-29 10:12:00 -0400

    init commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+
+function App() {
+  return (
+    <Router>
+      <nav className="p-4 bg-gray-200">
+        <Link to="/" className="mr-4">Home</Link>
+        <Link to="/about">About</Link>
+      </nav>
+      <div className="p-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/About.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h2 className="text-2xl font-bold">About Page</h2>
+      <p>This is the about page.</p>
+    </div>
+  );
+}
+
+export default About;
