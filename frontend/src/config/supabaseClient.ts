f9971f31b8374b5266fa9053d46fa0eaec7bdfef
Isaac Gemal <isaacgemal@gmail.com>
2024-10-29 20:34:31 -0400

    Implement initial fullstack integration - supabase integration in progress

---
+++ frontend/src/config/supabaseClient.ts
@@ -0,0 +1,8 @@
+import { createClient, SupabaseClient } from "@supabase/supabase-js";
+
+const supabaseUrl: string = import.meta.env.VITE_SUPABASE_URL as string;
+const supabaseKey: string = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
+
+const supabase: SupabaseClient = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
